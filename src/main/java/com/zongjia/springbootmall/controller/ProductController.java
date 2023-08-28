6c4711c067057099db6ce13cac4e7d7fbe0c9756
ZONG-JIA GU <kabuto412rock@gmail.com>
2023-08-28 21:56:19 +0800

    「商品功能 - 實作新增商品」

--- src/main/java/com/zongjia/springbootmall/controller/ProductController.java
+++ src/main/java/com/zongjia/springbootmall/controller/ProductController.java
@@ -1,20 +1,21 @@
 package com.zongjia.springbootmall.controller;
 
+import com.zongjia.springbootmall.dto.ProductRequest;
 import com.zongjia.springbootmall.model.Product;
 import com.zongjia.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/product/{productId}")
+    @GetMapping("/products/{productId}")
     public ResponseEntity<Product> getProductById(@PathVariable Integer productId) {
         Product product = productService.getProductById(productId);
 
@@ -22,5 +23,14 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.OK).body(product);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@Valid  @RequestBody ProductRequest productRequest) {
+        Integer productId = productService.createProduct(productRequest);
+
+        Product product = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 }
--- src/main/java/com/zongjia/springbootmall/service/ProductService.java
+++ src/main/java/com/zongjia/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.zongjia.springbootmall.service;
 
+import com.zongjia.springbootmall.dto.ProductRequest;
 import com.zongjia.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
