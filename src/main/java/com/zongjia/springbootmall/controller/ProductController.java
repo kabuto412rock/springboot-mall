982e113b1ff09d37ceeab0ae502b20b99ee56a84
ZONG-JIA GU <kabuto412rock@gmail.com>
2023-09-01 11:00:09 +0800

    商品功能 - 實作查詢商品列表 (分頁)

--- src/main/java/com/zongjia/springbootmall/controller/ProductController.java
+++ src/main/java/com/zongjia/springbootmall/controller/ProductController.java
@@ -5,15 +5,18 @@ import com.zongjia.springbootmall.dto.ProductQueryParams;
 import com.zongjia.springbootmall.dto.ProductRequest;
 import com.zongjia.springbootmall.model.Product;
 import com.zongjia.springbootmall.service.ProductService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -27,13 +30,19 @@ public class ProductController {
 
             // 排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // 分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0 ) Integer offset
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
-        productQueryParams.setSort(sort);
+        productQueryParams .setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> products = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(products);
