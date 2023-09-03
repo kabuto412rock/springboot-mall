994ff8582615ae260e4df95f0182b751d12c482a
ZONG-JIA GU <kabuto412rock@gmail.com>
2023-09-03 12:11:03 +0800

    帳號功能 - 登入

--- src/main/java/com/zongjia/springbootmall/controller/UserController.java
+++ src/main/java/com/zongjia/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.zongjia.springbootmall.controller;
 
+import com.zongjia.springbootmall.dto.UserLoginRequest;
 import com.zongjia.springbootmall.dto.UserRegisterRequest;
 import com.zongjia.springbootmall.model.User;
 import com.zongjia.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/zongjia/springbootmall/service/UserService.java
+++ src/main/java/com/zongjia/springbootmall/service/UserService.java
@@ -1,9 +1,13 @@
 package com.zongjia.springbootmall.service;
 
+import com.zongjia.springbootmall.dto.UserLoginRequest;
 import com.zongjia.springbootmall.dto.UserRegisterRequest;
 import com.zongjia.springbootmall.model.User;
 
 public interface UserService {
     User getUserById(Integer userId);
+
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
